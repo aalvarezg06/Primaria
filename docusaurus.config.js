aa78de2c9a061016313af235e5a926b92979b5d5
aalvarezg06 <aalvarezg06@eduucarex.es>
2026-05-15 19:41:00 +0200

    Configurar Docusaurus para despliegue en GitHub Pages

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -20,15 +20,16 @@ const config = {
   },
 
   // Set the production url of your site here
-  url: 'https://your-docusaurus-site.example.com',
+  url: 'https://aalvarezg06.github.io',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/',
+  baseUrl: '/Primaria/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'facebook', // Usually your GitHub org/user name.
-  projectName: 'docusaurus', // Usually your repo name.
+  organizationName: 'aalvarezg06', // Usually your GitHub org/user name.
+  projectName: 'Primaria', // Usually your repo name.
+  trailingSlash: false,
 
   onBrokenLinks: 'throw',
 
